3ef0cd6990e651611426ae9e88302326bd8b4f79
andreyfranca <andreygfranca@gmail.com>
2025-02-05 10:49:24 -0300

    refactor serializers

--- src/dynamodb/serializers.rs
+++ src/dynamodb/serializers.rs
@@ -1,4 +1,3 @@
-
 use bytes::Bytes;
 use serde::{ser::SerializeSeq, Deserialize, Serializer};
 
@@ -6,15 +5,16 @@ pub fn serialize_list<S>(bytes_list: &Option<Vec<Bytes>>, serializer: S) -> Resu
 where
     S: Serializer,
 {
-    if let Some(bytes_list) = bytes_list {
-        let encoded_list: Vec<String> = bytes_list.iter().map(|b| base64::encode(b)).collect();
-        let mut seq = serializer.serialize_seq(Some(encoded_list.len()))?;
-        for encoded in encoded_list {
-            seq.serialize_element(&encoded)?;
+    match bytes_list {
+        Some(bytes_list) => {
+            let encoded_list: Vec<String> = bytes_list.iter().map(|b| base64::encode(b)).collect();
+            let mut seq = serializer.serialize_seq(Some(encoded_list.len()))?;
+            for encoded in encoded_list {
+                seq.serialize_element(&encoded)?;
+            }
+            seq.end()
         }
-        seq.end()
-    } else {
-        serializer.serialize_none()
+        None => serializer.serialize_none(),
     }
 }
 
@@ -23,15 +23,10 @@ where
     D: serde::Deserializer<'de>,
 {
     let s: Vec<String> = Vec::deserialize(deserializer)?;
-    let result: Result<Vec<Bytes>, _> = s
-        .into_iter()
-        .map(|s| {
-            base64::decode(s)
-                .map(Bytes::from)
-                .map_err(serde::de::Error::custom)
-        })
-        .collect();
-    result.map(Some)
+    s.into_iter()
+        .map(|s| base64::decode(s).map(Bytes::from).map_err(serde::de::Error::custom))
+        .collect::<Result<Vec<_>, _>>()
+        .map(Some)
 }
 
 pub fn serialize<S>(bytes: &Option<Bytes>, serializer: S) -> Result<S::Ok, S::Error>
@@ -48,13 +43,7 @@ pub fn deserialize<'de, D>(deserializer: D) -> Result<Option<Bytes>, D::Error>
 where
     D: serde::Deserializer<'de>,
 {
-    let s: Option<String> = Option::deserialize(deserializer)?;
-    match s {
-        Some(s) => base64::decode(s)
-            .map(Bytes::from)
-            .map(Some)
-            .map_err(serde::de::Error::custom),
-        None => Ok(None),
-    }
+    Option::<String>::deserialize(deserializer)?
+        .map(|s| base64::decode(s).map(Bytes::from).map_err(serde::de::Error::custom))
+        .transpose()
 }
-
